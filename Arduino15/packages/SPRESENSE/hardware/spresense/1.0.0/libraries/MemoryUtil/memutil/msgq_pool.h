d9d30ff2689b20416da2ac4a062495283e95cae3
ryuta sakane <Ryuuta.Sakane@sony.com>
2019-12-05 14:45:20 +0900

    Support synthesizer

--- Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/libraries/MemoryUtil/memutil/msgq_pool.h
+++ Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/libraries/MemoryUtil/memutil/msgq_pool.h
@@ -39,40 +39,44 @@
 
 #include "msgq_id.h"
 
-extern const MsgQueDef MsgqPoolDefs[NUM_MSGQ_POOLS] = {
-   /* n_drm, n_size, n_num, h_drm, h_size, h_num */
+extern const MsgQueDef MsgqPoolDefs[NUM_MSGQ_POOLS] =
+{
+  /* n_drm, n_size, n_num, h_drm, h_size, h_num */
+
   { 0x00000000, 0, 0, 0x00000000, 0, 0, 0 }, /* MSGQ_NULL */
-  { 0xfd880, 88, 10, 0xffffffff, 0, 0 }, /* MSGQ_AUD_MGR */
-  { 0xfdbf0, 40, 2, 0xffffffff, 0, 0 }, /* MSGQ_AUD_APP */
-  { 0xfdc40, 20, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_DSP */
-  { 0xfdca4, 20, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_SUB_DSP */
-  { 0xfdd08, 48, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_PLY */
-  { 0xfddf8, 20, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_PFDSP0 */
-  { 0xfde5c, 20, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_PFDSP1 */
-  { 0xfdec0, 48, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_SUB_PLY */
-  { 0xfdfb0, 48, 8, 0xffffffff, 0, 0 }, /* MSGQ_AUD_OUTPUT_MIX */
-  { 0xfe130, 32, 16, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RND_PLY */
-  { 0xfe330, 16, 2, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RND_PLY_SYNC */
-  { 0xfe350, 32, 16, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RND_SUB */
-  { 0xfe550, 16, 2, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RND_SUB_SYNC */
-  { 0xfe570, 48, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RECORDER */
-  { 0xfe660, 48, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RECOGNIZER */
-  { 0xfe750, 24, 16, 0xffffffff, 0, 0 }, /* MSGQ_AUD_CAP */
-  { 0xfe8d0, 16, 2, 0xffffffff, 0, 0 }, /* MSGQ_AUD_CAP_SYNC */
-  { 0xfe8f0, 52, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_SOUND_EFFECT */
-  { 0xfe9f4, 20, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RCG_CMD */
-  { 0xfea58, 24, 16, 0xffffffff, 0, 0 }, /* MSGQ_AUD_CAP_MIC */
-  { 0xfebd8, 16, 2, 0xffffffff, 0, 0 }, /* MSGQ_AUD_CAP_MIC_SYNC */
-  { 0xfebf8, 24, 16, 0xffffffff, 0, 0 }, /* MSGQ_AUD_CAP_I2S */
-  { 0xfed78, 16, 2, 0xffffffff, 0, 0 }, /* MSGQ_AUD_CAP_I2S_SYNC */
-  { 0xfed98, 32, 16, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RND_SPHP */
-  { 0xfef98, 16, 2, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RND_SPHP_SYNC */
-  { 0xfefb8, 32, 16, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RND_I2S */
-  { 0xff1b8, 16, 2, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RND_I2S_SYNC */
-  { 0xff1d8, 48, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_FRONTEND */
-  { 0xff2c8, 20, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_PREDSP */
-  { 0xff32c, 20, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RCGDSP */
-  { 0xff390, 80, 16, 0xffffffff, 0, 0 }, /* MSGQ_SEN_MGR */
+  { 0xfd908, 88, 10, 0xffffffff, 0, 0 }, /* MSGQ_AUD_MGR */
+  { 0xfdc78, 40, 2, 0xffffffff, 0, 0 }, /* MSGQ_AUD_APP */
+  { 0xfdcc8, 20, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_DSP */
+  { 0xfdd2c, 20, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_SUB_DSP */
+  { 0xfdd90, 48, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_PLY */
+  { 0xfde80, 20, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_PFDSP0 */
+  { 0xfdee4, 20, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_PFDSP1 */
+  { 0xfdf48, 48, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_SUB_PLY */
+  { 0xfe038, 48, 8, 0xffffffff, 0, 0 }, /* MSGQ_AUD_OUTPUT_MIX */
+  { 0xfe1b8, 32, 16, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RND_PLY */
+  { 0xfe3b8, 16, 2, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RND_PLY_SYNC */
+  { 0xfe3d8, 32, 16, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RND_SUB */
+  { 0xfe5d8, 16, 2, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RND_SUB_SYNC */
+  { 0xfe5f8, 48, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RECORDER */
+  { 0xfe6e8, 48, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RECOGNIZER */
+  { 0xfe7d8, 24, 16, 0xffffffff, 0, 0 }, /* MSGQ_AUD_CAP */
+  { 0xfe958, 16, 2, 0xffffffff, 0, 0 }, /* MSGQ_AUD_CAP_SYNC */
+  { 0xfe978, 52, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_SOUND_EFFECT */
+  { 0xfea7c, 20, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RCG_CMD */
+  { 0xfeae0, 24, 16, 0xffffffff, 0, 0 }, /* MSGQ_AUD_CAP_MIC */
+  { 0xfec60, 16, 2, 0xffffffff, 0, 0 }, /* MSGQ_AUD_CAP_MIC_SYNC */
+  { 0xfec80, 24, 16, 0xffffffff, 0, 0 }, /* MSGQ_AUD_CAP_I2S */
+  { 0xfee00, 16, 2, 0xffffffff, 0, 0 }, /* MSGQ_AUD_CAP_I2S_SYNC */
+  { 0xfee20, 32, 16, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RND_SPHP */
+  { 0xff020, 16, 2, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RND_SPHP_SYNC */
+  { 0xff040, 32, 16, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RND_I2S */
+  { 0xff240, 16, 2, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RND_I2S_SYNC */
+  { 0xff260, 48, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_FRONTEND */
+  { 0xff350, 20, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_PREDSP */
+  { 0xff3b4, 20, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_RCGDSP */
+  { 0xff418, 48, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_SYNTHESIZER */
+  { 0xff508, 20, 5, 0xffffffff, 0, 0 }, /* MSGQ_AUD_OSCDSP */
+  { 0xff56c, 80, 16, 0xffffffff, 0, 0 }, /* MSGQ_SEN_MGR */
 };
 
 #endif /* MSGQ_POOL_H_INCLUDED */
